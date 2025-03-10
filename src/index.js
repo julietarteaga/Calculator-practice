4c77e27a2f6f51c8836425e01031ca0b93875729
Julieta Arteaga <A01637444@tec.mx>
2025-03-09 23:55:03 -0600

    Added substraction functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-function calculate(){
-    const num1 = document.getElementById("num1").value ;
-    const num2 = document.getElementById("num2").value ;
+function calculate(operation) {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const operatorElement = document.getElementById("operator");
+    const resultElement = document.getElementById("result");
+
+    if (isNaN(num1) || isNaN(num2)) {
+        resultElement.innerText = "Please enter valid numbers!";
+        return;
+    }
 
     let result;
-    result = Number(num1) + Number(num2);
+    if (operation === "add") {
+        result = num1 + num2;
+        operatorElement.innerText = "+";
+    } else if (operation === "sub") {
+        result = num1 - num2;
+        operatorElement.innerText = "-";
+    }
 
-    document.getElementById("result").innerText = "Result: "+ result;
+    resultElement.innerText = "Result: " + result;
 }
 
-function resetFields(){
+function resetFields() {
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
-    document.getElementById("operator").value = "+";
-    document.getElementById("result").innerText = "Result: ";
-
+    document.getElementById("operator").innerText = "";
+    document.getElementById("result").innerText = "Result:";
 }
